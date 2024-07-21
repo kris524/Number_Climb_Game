151f31ca3f23b4f4e04faaf9b3af62b034604764
Kris <kristian_dilov@abv.bg>
2024-07-21 22:51:21 +0100

    Initial Commit

---
+++ number_climb/number_climbApp.swift
@@ -0,0 +1,17 @@
+//
+//  number_climbApp.swift
+//  number_climb
+//
+//  Created by Kristiyan Dilov on 21/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct number_climbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
